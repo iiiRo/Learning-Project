fcb82a7af3c8ba3fd90a74d9b0d8e62c741e4c4c
Ro <lyzko_ruslan@ukr.net>
2019-03-10 14:15:50 +0200

    This's my first commit

---
+++ Tutorial project/javascript.js
@@ -0,0 +1,35 @@
+
+let someGuy = {
+    name: "Bob",
+    gender: "male"
+};
+
+// alert(someGuy.name);
+
+//
+// alert(someGuy2[0]); 
+// confirm('are u rdy?');
+//let userAnswer = prompt('Do you want to close a prompt?', 'Yep');
+//console.log(typeof(someGuy2));
+let 
+a = 100500,
+b = 200500;
+console.log((++a),(--b));
+console.log(5/2);
+if (("5" == 5) && ("5" === 5) )
+{
+    console.log('Y')
+}
+else {
+
+    console.log('N')
+};
+let
+c = true,
+d = true;
+
+console.log(c && d);
+
+
+
+
---
+++ Tutorial project/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let someGuy = {
+    name: "Bob",
+    gender: "male"
+};
+
+alert(someGuy.name);
+
+let someGuy2 = ['John','male'];
+alert(someGuy2[0]);
